ab350abc652c0be7cbe9e31facbb84175c9daeba
David Barina <ibarina@fit.vutbr.cz>
2019-08-05 17:11:29 +0200

    uint128_t into prescreen.c

--- prescreen.c
+++ prescreen.c
@@ -9,9 +9,13 @@
 #include <assert.h>
 #include <limits.h>
 
+#include "wideint.h"
+
 #define LUT_SIZE 32
 
 unsigned long g_lut[LUT_SIZE];
+
+uint128_t g_lut128[LUT_SIZE];
 
 /* 3^n, in runtime */
 unsigned long lut_rt(unsigned long n)
@@ -27,12 +31,26 @@ unsigned long lut_rt(unsigned long n)
 	return r;
 }
 
+uint128_t lut_rt128(uint128_t n)
+{
+	uint128_t r = 1;
+
+	for (; n > 0; --n) {
+		assert( r <= UINT128_MAX / 3 );
+
+		r *= 3;
+	}
+
+	return r;
+}
+
 void init_lut()
 {
 	unsigned long a;
 
 	for (a = 0; a < LUT_SIZE; ++a) {
 		g_lut[a] = lut_rt(a);
+		g_lut128[a] = lut_rt128(a);
 	}
 }
 
@@ -42,6 +60,13 @@ static unsigned long lut(unsigned long n)
 	assert( n < LUT_SIZE );
 
 	return g_lut[n];
+}
+
+static uint128_t lut128(uint128_t n)
+{
+	assert( n < LUT_SIZE );
+
+	return g_lut128[n];
 }
 
 /* check convergence */
@@ -64,6 +89,51 @@ void check(unsigned long n)
 		n >>= e;
 		assert( n <= ULONG_MAX >> 2*e );
 		n *= lut(e);
+
+		n--;
+	} while (1);
+}
+
+static uint128_t ctz128(uint128_t n)
+{
+	switch (sizeof(uint128_t)) {
+		case 2*sizeof(unsigned long): {
+			if ((unsigned long)n == 0)
+				return (sizeof(unsigned long) * CHAR_BIT) + __builtin_ctzl((unsigned long)(n >> (sizeof(unsigned long) * CHAR_BIT)));
+			else
+				return __builtin_ctzl((unsigned long)n);
+		}
+		default:
+			abort();
+	}
+}
+
+void prescreen128(uint128_t n, uint128_t n_sup, uint128_t e)
+{
+	uint128_t n0 = n;
+	uint128_t e0 = e;
+
+goto entry;
+	do {
+		n >>= ctz128(n);
+
+		if (n == 1)
+			return;
+
+		n++;
+
+		e = ctz128(n);
+		n >>= e;
+
+		/* now we have (n,e) pair */
+
+		/* all (n,e) with n < n_sup and e < e0 have already been checked */
+		if ( (n < n_sup && e < e0) || (n < n0 && e == e0) )
+			return;
+entry:
+
+		assert( n <= UINT128_MAX >> 2*e );
+		n *= lut128(e);
 
 		n--;
 	} while (1);
@@ -92,7 +162,10 @@ goto entry;
 		if ( (n < n_sup && e < e0) || (n < n0 && e == e0) )
 			return;
 entry:
-
+		if ( n > ULONG_MAX >> 2*e) {
+			prescreen128(n0, n_sup, e0);
+			return;
+		}
 		assert( n <= ULONG_MAX >> 2*e );
 		n *= lut(e);
 
@@ -104,7 +177,12 @@ int main(int argc, char *argv[])
 {
 	unsigned long e;
 	unsigned long n;
-	unsigned long n_sup = (argc > 1) ? (unsigned long)atol(argv[1]) : (1UL<<24);
+	unsigned long n_sup = (argc > 1) ? (unsigned long)atol(argv[1]) : (1UL<<32);
+
+	assert( ctz128( INT128_C(1)<<15 ) == 15 );
+	assert( ctz128( INT128_C(1)<<35 ) == 35 );
+	assert( ctz128( INT128_C(1)<<65 ) == 65 );
+	assert( ctz128( INT128_C(1)<<127 ) == 127 );
 
 	init_lut();
 #if 0
---
+++ wideint.h
@@ -0,0 +1,32 @@
+/**
+ * Support for 128-bit integers.
+ */
+
+#ifndef WIDEINT_WIDEINT_H_
+#define WIDEINT_WIDEINT_H_
+
+/* all GNU compilers */
+#ifdef __GNUC__
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-pedantic"
+typedef unsigned __int128 uint128_t;
+#pragma GCC diagnostic pop
+
+#define UINT128_C(n) ( (uint128_t)n )
+#define UINT128_MAX ( ~UINT128_C(0) )
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-pedantic"
+typedef __int128 int128_t;
+#pragma GCC diagnostic pop
+
+#define INT128_C(n) ( (int128_t)n )
+#define INT128_MAX ( ((INT128_C(1)<<(128-2))-1)*2+1 )
+#define INT128_MIN ( -INT128_MAX - 1 )
+
+#else
+#	error "Unsupported compiler"
+#endif
+
+#endif /* WIDEINT_WIDEINT_H_ */
