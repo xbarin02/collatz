20791da3eb75172e260086c3d0280d151366d0a6
David Barina <ibarina@fit.vutbr.cz>
2019-08-06 15:09:20 +0200

    collatz4: all numbers below 2^32 under 2 secs

--- collatz4.c
+++ collatz4.c
@@ -66,7 +66,7 @@ void check(unsigned long n)
 		/* (n,e) pair */
 
 		/* all (n,e) below the following limits have already been checked for convergence */
-		if (e < 30 && n < 1UL<<29)
+		if (e < 41 && n < 1UL<<30)
 			return;
 
 		assert( n <= ULONG_MAX >> 2*e );
