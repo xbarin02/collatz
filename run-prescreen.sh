b6d2252fd0f81f59655a14298ed154b050db9820
David Barina <ibarina@fit.vutbr.cz>
2019-08-08 17:55:59 +0200

    fix run script

--- run-prescreen.sh
+++ run-prescreen.sh
@@ -6,4 +6,4 @@ BITS=${2:-32}
 echo "THREADS: $THREADS"
 echo "BITS: $BITS"
 
-make prescreen && OMP_NUM_THREADS=$THREADS OMP_SCHEDULE=static stdbuf -o0 -e0 ./prescreen | tee prescreen-2^$BITS.txt
+make prescreen && OMP_NUM_THREADS=$THREADS OMP_SCHEDULE=static stdbuf -o0 -e0 ./prescreen $BITS | tee prescreen-2^$BITS.txt
