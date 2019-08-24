1c6063be3eda86037e63d4e2d0ef04acb06a43b9
David Barina <ibarina@fit.vutbr.cz>
2019-08-24 15:32:05 +0200

    task2: notes

--- collatz_task2.c
+++ collatz_task2.c
@@ -46,7 +46,8 @@ static void check(uint128_t n)
 	int e;
 
 	do {
-#if 1
+#if 0
+		/* Christian Hercher, Uber die Lange nicht-trivialer Collatz-Zyklen, Artikel in der Zeitschrift "Die Wurzel" Hefte 6 und 7/2018 */
 		if (n <= UINT128_C(87)<<60)
 			return;
 #endif
@@ -56,10 +57,11 @@ static void check(uint128_t n)
 
 		n >>= e;
 
-		/* (n,e) pair */
+		/* now we have an (n,e) pair */
 #if 1
 		/* switch to 64-bit arithmetic */
 		if ( n < UINT128_C(1)<<64 && e < LUT_SIZE ) {
+			/* when the result of n*3^e and thus also odd_part(n*3^e-1) fits the 64-bit unsigned long, it is surely less than 87*2^60 */
 			return;
 		}
 #endif
