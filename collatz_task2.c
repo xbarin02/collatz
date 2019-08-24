c226d36d6104ac82a506e0f90920e82880e47127
David Barina <ibarina@fit.vutbr.cz>
2019-08-24 14:39:48 +0200

    task2: simpler code

--- collatz_task2.c
+++ collatz_task2.c
@@ -14,11 +14,7 @@
 #define LUT_SIZE 41
 #define LUT_SIZE128 81
 
-typedef union {
-	uint128_t ull;
-} uint128_u;
-
-uint128_u g_lutu[LUT_SIZE128];
+uint128_t g_lut[LUT_SIZE128];
 
 /* 3^n */
 uint128_t pow3x(uint128_t n)
@@ -39,45 +35,45 @@ void init_lut()
 	unsigned long a;
 
 	for (a = 0; a < LUT_SIZE128; ++a) {
-		g_lutu[a].ull = pow3x(a);
+		g_lut[a] = pow3x(a);
 	}
 }
 
 /* check convergence */
-static void check(uint128_u n)
+static void check(uint128_t n)
 {
-	uint128_u n0 = n;
+	uint128_t n0 = n;
 	int e;
 
 	do {
 #if 1
-		if (n.ull <= UINT128_C(87)<<60)
+		if (n <= UINT128_C(87)<<60)
 			return;
 #endif
-		n.ull++;
+		n++;
 
-		e = __builtin_ctzx(n.ull);
+		e = __builtin_ctzx(n);
 
-		n.ull >>= e;
+		n >>= e;
 
 		/* (n,e) pair */
 #if 1
 		/* switch to 64-bit arithmetic */
-		if ( n.ull < UINT128_C(1)<<64 && e < LUT_SIZE ) {
+		if ( n < UINT128_C(1)<<64 && e < LUT_SIZE ) {
 			return;
 		}
 #endif
-		assert( n.ull <= UINT128_MAX >> 2*e && e < LUT_SIZE128 && "overflow" );
+		assert( n <= UINT128_MAX >> 2*e && e < LUT_SIZE128 && "overflow" );
 
-		n.ull *= g_lutu[e].ull;
+		n *= g_lut[e];
 
-		n.ull--;
+		n--;
 
-		n.ull >>= __builtin_ctzx(n.ull);
+		n >>= __builtin_ctzx(n);
 
 		/* now we have a single n */
 
-		if (n.ull < n0.ull)
+		if (n < n0)
 			return;
 	} while (1);
 }
@@ -86,7 +82,7 @@ static void check(uint128_u n)
 
 int main(int argc, char *argv[])
 {
-	uint128_u n;
+	uint128_t n;
 	uint128_t n_sup;
 	unsigned long task_id = (argc > 1) ? (unsigned long)atol(argv[1]) : 0;
 
@@ -97,15 +93,15 @@ int main(int argc, char *argv[])
 
 	/* start at */
 	/* n of the form 4n+3 */
-	n.ull = ( UINT128_C(task_id) << TASK_SIZE ) + 3;
+	n     = ( UINT128_C(task_id) << TASK_SIZE ) + 3;
 	n_sup = ( UINT128_C(task_id) << TASK_SIZE ) + 3 + (1UL << TASK_SIZE);
 
-	printf("RANGE 0x%016lx:%016lx .. 0x%016lx:%016lx\n", (unsigned long)(n.ull>>64), (unsigned long)n.ull, (unsigned long)(n_sup>>64), (unsigned long)n_sup);
+	printf("RANGE 0x%016lx:%016lx .. 0x%016lx:%016lx\n", (unsigned long)(n>>64), (unsigned long)n, (unsigned long)(n_sup>>64), (unsigned long)n_sup);
 
 	init_lut();
 
 	/* #pragma omp parallel for */
-	for (; n.ull < n_sup; n.ull += 4) {
+	for (; n < n_sup; n += 4) {
 		check(n);
 	}
 
