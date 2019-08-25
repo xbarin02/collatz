7a4413e1133baf3d7861e507bbd4040de06352a0
David Barina <ibarina@fit.vutbr.cz>
2019-08-25 09:34:37 +0200

    deleted old files

--- run-prescreen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-THREADS=${1:-1}
-BITS=${2:-32}
-
-echo "THREADS: $THREADS"
-echo "BITS: $BITS"
-
-make prescreen && OMP_NUM_THREADS=$THREADS OMP_SCHEDULE=static stdbuf -o0 -e0 ./prescreen $BITS | tee prescreen-2^$BITS.txt
