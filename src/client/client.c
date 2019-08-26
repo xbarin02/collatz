153c7603d94a4bf5b02ed25c757b84605db3357d
David Barina <ibarina@fit.vutbr.cz>
2019-08-26 19:31:20 +0200

    client connect()

--- src/client/client.c
+++ src/client/client.c
@@ -4,6 +4,12 @@
 #ifdef _OPENMP
 	#include <omp.h>
 #endif
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <strings.h>
 
 int threads_get_thread_id()
 {
@@ -12,6 +18,28 @@ int threads_get_thread_id()
 #else
 	return 0;
 #endif
+}
+
+const char *servername = "pcbarina2.fit.vutbr.cz";
+
+void init_sockaddr(struct sockaddr_in *name, const char *hostname, uint16_t port)
+{
+	struct hostent *hostinfo;
+
+	assert(name);
+
+	bzero(name, sizeof(struct sockaddr_in));
+
+	name->sin_family = AF_INET;
+	name->sin_port = htons(port);
+	hostinfo = gethostbyname(hostname);
+
+	if (hostinfo == NULL) {
+		fprintf(stderr, "gethostbyname failure: %s\n", servername);
+		abort();
+	}
+
+	name->sin_addr = *(struct in_addr *) hostinfo->h_addr_list[0];
 }
 
 int main(int argc, char *argv[])
@@ -27,9 +55,27 @@ int main(int argc, char *argv[])
 		printf("thread %i started\n", threads_get_thread_id());
 
 		do {
+			int fd = socket(AF_INET, SOCK_STREAM, 0);
+			struct sockaddr_in server_addr;
+
+			if (fd < 0) {
+				/* errno is set appropriately */
+				perror("socket");
+				abort();
+			}
+
+			init_sockaddr(&server_addr, servername, 5006);
+
+			if (connect(fd,(struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+				/* errno is set appropriately */
+				perror("connect");
+				abort();
+			}
+
 			/* TODO get assignment from server */
 			/* TODO spawn sub-process */
 			/* TODO send the result back to server */
+			close(fd);
 		} while (1);
 	}
 
