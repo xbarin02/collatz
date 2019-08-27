73d3334519cce6ae7ac8c3ef6c89510ee3ec36c1
David Barina <ibarina@fit.vutbr.cz>
2019-08-27 13:54:02 +0200

    server: update

--- src/server/server.c
+++ src/server/server.c
@@ -5,29 +5,150 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <netdb.h>
-/*#include <arpa/inet.h>*/
 #include <strings.h>
+#include <string.h>
+#include <assert.h>
+#include <signal.h>
+#include <errno.h>
+
+static volatile int quit = 0;
+
+void sigint_handler(int i)
+{
+	(void)i;
+
+	quit = 1;
+}
+
 
 const uint16_t serverport = 5006;
+
+int init_sockaddr(struct sockaddr_in *name, uint16_t port)
+{
+	assert(name);
+
+	bzero(name, sizeof(struct sockaddr_in));
+
+	name->sin_family = AF_INET;
+	name->sin_addr.s_addr = INADDR_ANY;
+	name->sin_port = htons(port);
+
+	return 0;
+}
+
+ssize_t write_(int fd, const char *buf, size_t count)
+{
+	ssize_t written = 0;
+
+	while ((size_t)written < count) {
+		ssize_t t = write(fd, buf+written, count - written);
+
+		if (t < 0) {
+			/* errno is set appropriately */
+			return -1;
+		}
+		if (t == 0 && (size_t)(written + t) != count) {
+			/* zero indicates nothing was written */
+			return -1;
+		}
+
+		written += t;
+	}
+
+	return written;
+}
+
+ssize_t read_(int fd, char *buf, size_t count)
+{
+	ssize_t readen = 0; /* read was already taken */
+
+	while ((size_t)readen < count) {
+		ssize_t t = read(fd, buf + readen, count - readen);
+
+		if (t < 0) {
+			/*  errno is set appropriately */
+			return -1;
+		}
+		if (t == 0 && (size_t)(readen + t) != count) {
+			/* zero indicates end of file */
+			return -1;
+		}
+
+		readen += t;
+	}
+
+	return readen;
+}
+
+int write_assignment_no(int fd, uint64_t n)
+{
+	uint32_t nh, nl;
+
+	nh = (uint32_t)(n >> 32);
+	nl = (uint32_t)(n);
+
+	nh = htonl(nh);
+	nl = ntohl(nl);
+
+	if (write_(fd, (void *)&nh, 4) < 0) {
+		return -1;
+	}
+
+	if (write_(fd, (void *)&nl, 4) < 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int read_message(int fd)
+{
+	char msg[4];
+
+	if (read_(fd, msg, 4) < 0) {
+		return -1;
+	}
+
+	/* unsupported protocol */
+	if (msg[3] != 0) {
+		return -1;
+	}
+
+	if (strcmp(msg, "REQ") == 0) {
+		/* requested assignment */
+		unsigned long n = rand();
+
+		printf("assignment requested\n");
+
+		assert( sizeof(uint64_t) == sizeof(unsigned long) );
+
+		write_assignment_no(fd, (uint64_t)n);
+	}
+
+	return 0;
+}
 
 int main(/*int argc, char *argv[]*/)
 {
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in serv_addr;
+	struct sockaddr_in server_addr;
+	int reuse = 1;
+
+	signal(SIGINT, sigint_handler);
 
 	if (fd < 0) {
 		perror("socket");
 		abort();
 	}
 
-	/* TODO setsockospt */
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&reuse, (socklen_t)sizeof(reuse)) < 0) {
+		perror("setsockopt");
+		abort();
+	}
 
-	bzero(&serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(serverport);
+	init_sockaddr(&server_addr, serverport);
 
-	if (bind(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+	if (bind(fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
 		perror("bind");
 		abort();
 	}
@@ -37,13 +158,31 @@ int main(/*int argc, char *argv[]*/)
 		abort();
 	}
 
+	printf("listening...\n");
+
 	while (1) {
 		int cl_fd = accept(fd, NULL, NULL);
 
+		if (-1 == cl_fd) {
+			if (errno == EINTR) {
+				if (quit)
+					break;
+			}
+		}
+
 		printf("connected\n");
 
+		read_message(cl_fd);
+
 		close(cl_fd);
+
+		if (quit)
+			break;
 	}
+
+	printf("closing server socket...\n");
+
+	close(fd);
 
 	return 0;
 }
