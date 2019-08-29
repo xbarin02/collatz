55b17da29caf144d7cf876c377382a082ead10c2
David Barina <ibarina@fit.vutbr.cz>
2019-08-29 15:16:26 +0200

    SGE script

---
+++ sge_submit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+#$ -S /bin/bash
+#$ -N collatz
+#$ -M ibarina@fit.vutbr.cz
+#$ -m a
+#$ -o /mnt/matylda1/ibarina/sge/collatz/out
+#$ -e /mnt/matylda1/ibarina/sge/collatz/err
+#$ -q all.q@@stable,all.q@@gpu
+#$ -tc 2000
+
+set -u
+set -e
+
+export LANG=C
+
+echo "hostname=$(hostname)"
+echo "pwd=$(pwd)"
+echo "TMPDIR=$TMPDIR"
+TMP=$(mktemp -d collatz.XXXXXXXX --tmpdir)
+echo "TMP=${TMP}"
+
+mkdir -p -- "$TMP"
+pushd -- "$TMP"
+
+SRCDIR=/mnt/matylda1/ibarina/sge/collatz/
+
+cp -r "${SRCDIR}" .
+
+cd collatz/src
+
+make -C worker
+make -C client
+
+cd client
+
+stdbuf -o0 -e0 ./client -1 1
+
+popd
+rm -rf -- "$TMP"
+
+echo "DONE"
