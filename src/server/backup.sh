0e28db10a161100644e5819a3734e749c3e3e133
David Barina <ibarina@fit.vutbr.cz>
2019-08-31 15:15:25 +0200

    server: backup script

--- src/server/backup.sh
+++ src/server/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mv -f assigned.000.map assigned.001.map
-mv -f complete.000.map complete.001.map
+mv -f -- assigned.000.map assigned.001.map
+mv -f -- complete.000.map complete.001.map
 
-cp assigned.map assigned.000.map
-cp complete.map complete.000.map
+cp -- assigned.map assigned.000.map
+cp -- complete.map complete.000.map
