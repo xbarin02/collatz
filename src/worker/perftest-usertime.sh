3c8935e8688fb317e72e99be9b3ed385f4906e7a
David Barina <ibarina@fit.vutbr.cz>
2019-09-03 08:02:52 +0200

    worker: script update

--- src/worker/perftest-usertime.sh
+++ src/worker/perftest-usertime.sh
@@ -6,13 +6,14 @@ PLOTFILE=usertime.txt
 
 rm -f -- "$PLOTFILE"
 
+t=32
 e=0
 n=1
 
 while :; do
-	USERTIME=$(\time ./worker $n 2>&1 | grep "user" | sed 's/user.*//')
-	echo -e "$e\t$n\t$USERTIME"
-	echo -e "$e\t$n\t$USERTIME" >> "$PLOTFILE"
+	R=$(\time ./worker -t $t $n 2>&1 | grep "user" | sed 's/user.*//')
+	echo -e "$e\t$n\t$R"
+	echo -e "$e\t$n\t$R" >> "$PLOTFILE"
 	n=$(($n * 2))
 	e=$(($e + 1))
 done
