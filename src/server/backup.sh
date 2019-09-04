177b96ecfe1c7fe2a3f8c28502767ab55e60e706
David Barina <ibarina@fit.vutbr.cz>
2019-09-04 08:54:07 +0200

    server: backup also checksums

--- src/server/backup.sh
+++ src/server/backup.sh
@@ -3,5 +3,9 @@
 test -e assigned.000.map && mv -f -- assigned.000.map assigned.001.map
 test -e complete.000.map && mv -f -- complete.000.map complete.001.map
 
+test -e checksums.000.dat && mv -f -- checksums.000.dat checksums.001.dat
+
 test -e assigned.map && cp -- assigned.map assigned.000.map
 test -e complete.map && cp -- complete.map complete.000.map
+
+test -e checksums.dat && cp -- checksums.dat checksums.000.dat
