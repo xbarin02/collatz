bad446a21e24dd3169f6fdcfa5a1941f492caee9
David Barina <ibarina@fit.vutbr.cz>
2019-10-19 11:22:48 +0200

    worker: mitigate the bug in computing checksums when 128-bit overflow occurs

--- src/worker/worker.c
+++ src/worker/worker.c
@@ -88,8 +88,35 @@ static mp_bitcnt_t mpz_ctz(const mpz_t n)
 }
 #endif
 
-static uint64_t g_check_sum_alpha = 0;
-static uint64_t g_check_sum_beta = 0;
+static uint64_t g_checksum_alpha = 0;
+static uint64_t g_checksum_beta = 0;
+
+static int check2(uint128_t n0, uint128_t n, int alpha)
+{
+	do {
+		if (alpha >= LUT_SIZE128 || n > UINT128_MAX / g_lut[alpha]) {
+			return 1;
+		}
+
+		n *= g_lut[alpha];
+
+		n--;
+
+		n >>= __builtin_ctzu128(n);
+
+		if (n < n0) {
+			return 0;
+		}
+
+		n++;
+
+		alpha = __builtin_ctzu128(n);
+
+		g_checksum_alpha += alpha;
+
+		n >>= alpha;
+	} while (1);
+}
 
 /* check convergence */
 static int check(uint128_t n)
@@ -106,14 +133,14 @@ static int check(uint128_t n)
 
 		alpha = __builtin_ctzu128(n);
 
-		g_check_sum_alpha += alpha;
+		g_checksum_alpha += alpha;
 
 		n >>= alpha;
 
 		/* now we have an (n,alpha) pair */
 
 		if (n > UINT128_MAX >> 2*alpha || alpha >= LUT_SIZE128) {
-			return 1;
+			return check2(n0, n, alpha);
 		}
 
 		n *= g_lut[alpha];
@@ -122,7 +149,7 @@ static int check(uint128_t n)
 
 		beta = __builtin_ctzu128(n);
 
-		g_check_sum_beta += beta;
+		g_checksum_beta += beta;
 
 		n >>= __builtin_ctzu128(n);
 
@@ -171,7 +198,7 @@ static void mpz_check(uint128_t n_)
 
 		alpha = mpz_ctz(n);
 
-		g_check_sum_alpha += alpha;
+		g_checksum_alpha += alpha;
 
 		/* n >>= alpha */
 		mpz_fdiv_q_2exp(n, n, alpha);
@@ -188,7 +215,7 @@ static void mpz_check(uint128_t n_)
 
 		beta = mpz_ctz(n);
 
-		g_check_sum_beta += beta;
+		g_checksum_beta += beta;
 
 		/* n >>= ctz(n) */
 		mpz_fdiv_q_2exp(n, n, beta);
@@ -307,7 +334,7 @@ int main(int argc, char *argv[])
 
 	printf("OVERFLOW 128 %" PRIu64 "\n", g_overflow_counter);
 
-	printf("CHECKSUM %" PRIu64 " %" PRIu64 "\n", g_check_sum_alpha, g_check_sum_beta);
+	printf("CHECKSUM %" PRIu64 " %" PRIu64 "\n", g_checksum_alpha, g_checksum_beta);
 
 	printf("HALTED\n");
 
