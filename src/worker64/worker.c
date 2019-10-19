a85589a61ec9647ac778a0b6c9196654e14f8a0a
David Barina <ibarina@fit.vutbr.cz>
2019-10-19 14:21:56 +0200

    worker64: assert

--- src/worker64/worker.c
+++ src/worker64/worker.c
@@ -196,6 +196,8 @@ int main(int argc, char *argv[])
 	printf("TASK_SIZE %" PRIu64 "\n", task_size);
 	printf("TASK_ID %" PRIu64 "\n", task_id);
 
+	assert(task_id <= (UINT64_MAX >> task_size));
+
 	/* n of the form 4n+3 */
 	n     = ((uint64_t)(task_id) << task_size) + 3;
 	n_sup = ((uint64_t)(task_id) << task_size) + 3 + (UINT64_C(1) << task_size);
