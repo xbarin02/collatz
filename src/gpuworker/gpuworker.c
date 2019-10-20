51fb6dac993aaf8fb590de3b9f0ec60ed05dd11b
David Barina <ibarina@fit.vutbr.cz>
2019-10-20 14:33:16 +0200

    gpuworker: mpz implementation, disable by default

--- src/gpuworker/gpuworker.c
+++ src/gpuworker/gpuworker.c
@@ -8,6 +8,9 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <CL/cl.h>
+#ifdef _USE_GMP
+#	include <gmp.h>
+#endif
 
 #include "compat.h"
 #include "wideint.h"
@@ -45,9 +48,22 @@ uint128_t pow3x(uint128_t n)
 	return r;
 }
 
+#ifdef _USE_GMP
+static void mpz_pow3(mpz_t r, unsigned long n)
+{
+	mpz_ui_pow_ui(r, 3UL, n);
+}
+#endif
+
 #define LUT_SIZE128 81
+#ifdef _USE_GMP
+#	define LUT_SIZEMPZ 512
+#endif
 
 uint128_t g_lut[LUT_SIZE128];
+#ifdef _USE_GMP
+mpz_t g_mpz_lut[LUT_SIZEMPZ];
+#endif
 
 void init_lut()
 {
@@ -56,7 +72,84 @@ void init_lut()
 	for (a = 0; a < LUT_SIZE128; ++a) {
 		g_lut[a] = pow3x((uint128_t)a);
 	}
+
+#ifdef _USE_GMP
+	for (a = 0; a < LUT_SIZEMPZ; ++a) {
+		mpz_init(g_mpz_lut[a]);
+		mpz_pow3(g_mpz_lut[a], (unsigned long)a);
+	}
+#endif
 }
+
+#ifdef _USE_GMP
+static mp_bitcnt_t mpz_ctz(const mpz_t n)
+{
+	return mpz_scan1(n, 0);
+}
+#endif
+
+#ifdef _USE_GMP
+static void mpz_init_set_u128(mpz_t rop, uint128_t op)
+{
+	uint64_t nh = (uint64_t)(op>>64);
+	uint64_t nl = (uint64_t)(op);
+
+	assert(sizeof(unsigned long) == sizeof(uint64_t));
+
+	mpz_init_set_ui(rop, (unsigned long)nh);
+	mpz_mul_2exp(rop, rop, (mp_bitcnt_t)64);
+	mpz_add_ui(rop, rop, (unsigned long)nl);
+}
+#endif
+
+#ifdef _USE_GMP
+static void mpz_check2(uint128_t n0_, uint128_t n_, int alpha_)
+{
+	mp_bitcnt_t alpha, beta;
+	mpz_t n;
+	mpz_t n0;
+
+	assert(alpha_ >= 0);
+	alpha = (mp_bitcnt_t)alpha_;
+
+	mpz_init_set_u128(n, n_);
+	mpz_init_set_u128(n0, n0_);
+
+	do {
+		if (alpha >= LUT_SIZEMPZ) {
+			abort();
+		}
+
+		/* n *= lut[alpha] */
+		mpz_mul(n, n, g_mpz_lut[alpha]);
+
+		/* n-- */
+		mpz_sub_ui(n, n, 1UL);
+
+		beta = mpz_ctz(n);
+
+		/* n >>= ctz(n) */
+		mpz_fdiv_q_2exp(n, n, beta);
+
+		if (mpz_cmp(n, n0) < 0) {
+			break;
+		}
+
+		/* n++ */
+		mpz_add_ui(n, n, 1UL);
+
+		alpha = mpz_ctz(n);
+
+		g_private_checksum_alpha += alpha;
+
+		/* n >>= alpha */
+		mpz_fdiv_q_2exp(n, n, alpha);
+	} while (1);
+
+	mpz_clear(n);
+	mpz_clear(n0);
+}
+#endif
 
 static void check2(uint128_t n0, uint128_t n, int alpha)
 {
