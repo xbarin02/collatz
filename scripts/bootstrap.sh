bfcbce942f65127bb01f09a0100732e9d87db3bc
David Barina <ibarina@fit.vutbr.cz>
2019-10-25 20:59:49 +0200

    scripts/bootstrap.sh: detect clang

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,10 @@ export SERVER_NAME=pcbarina.fit.vutbr.cz
 
 cd ~/collatz-$(hostname -s)/src
 
+if type clang > /dev/null 2> /dev/null && clang --version | grep -q "version [89]"; then
+	echo "INFO: clang available"
+fi
+
 make -C worker clean all
 make -C gpuworker clean all || echo "unable to build gpuworker"
 make -C mclient clean all
