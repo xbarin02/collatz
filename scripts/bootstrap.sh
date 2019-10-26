9e4df88957dd6a0f7f50f5777cb0a45e3ebf5cfe
David Barina <ibarina@fit.vutbr.cz>
2019-10-26 12:15:28 +0200

    scripts/bootstrap.sh: use clang when available

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,11 +4,13 @@ export SERVER_NAME=pcbarina.fit.vutbr.cz
 
 cd ~/collatz-$(hostname -s)/src
 
+CC=gcc
 if type clang > /dev/null 2> /dev/null && clang --version | grep -q "version [89]"; then
 	echo "INFO: clang available"
+	CC=clang
 fi
 
-make -C worker clean all
+make -C worker clean all CC=$CC
 make -C gpuworker clean all || echo "unable to build gpuworker"
 make -C mclient clean all
 
