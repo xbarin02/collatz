17faf443ac617e08481b6f3b747941223f68e31d
David Barina <ibarina@fit.vutbr.cz>
2019-11-01 12:46:18 +0100

    server: allow 0xff5b

--- src/server/server.c
+++ src/server/server.c
@@ -573,7 +573,7 @@ int read_message(int fd, int thread_id, const char *ipv4)
 			return -1;
 		}
 
-		if ((checksum>>23) != 196126 && (checksum>>24) != 0xff5c) {
+		if ((checksum>>23) != 196126 && (checksum>>24) != 0xff5c && (checksum>>24) != 0xff5b) {
 			message(ERR "suspicious checksum (%" PRIu64 ", 0x%" PRIx64 "), rejecting the result! (assignment %" PRIu64 ")\n", checksum, checksum, n);
 			return -1;
 		}
