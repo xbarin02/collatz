c0b024ac81eff03d3f38f0eac40cbf93e892c974
David Barina <ibarina@fit.vutbr.cz>
2019-11-20 11:08:00 +0100

    server: disable new checkusms (0xff5f)

--- src/server/server.c
+++ src/server/server.c
@@ -573,17 +573,17 @@ int read_message(int fd, int thread_id, const char *ipv4)
 			return -1;
 		}
 
-		if ((checksum>>23) != 196126 && (checksum>>24) != 0xff5f) {
+		if ((checksum>>23) != 196126 /*&& (checksum>>24) != 0xff5f*/) {
 			message(ERR "suspicious checksum (%" PRIu64 ", 0x%" PRIx64 "), done in %" PRIu64 " secs, rejecting the result! (assignment %" PRIu64 ")\n",
 				checksum, checksum, user_time, n);
 			return -1;
 		}
 
-		if ((checksum>>24) == 0xff5f && user_time < 30*60) {
+		/*if ((checksum>>24) == 0xff5f && user_time < 30*60) {
 			message(ERR "new checksum is not allowed for GPU (%" PRIu64 ", 0x%" PRIx64 "), done in %" PRIu64 " secs, rejecting the result! (assignment %" PRIu64 ")\n",
 				checksum, checksum, user_time, n);
 			return 0;
-		}
+		}*/
 
 		message(INFO "assignment returned: %" PRIu64 " (%" PRIu64 " overflows, time %" PRIu64 ":%02" PRIu64 ":%02" PRIu64 ", checksum 0x%016" PRIx64 ")\n",
 			n, overflow_counter, user_time/60/60, user_time/60%60, user_time%60, checksum);
