44360f665d671b6dc2f473f114c4505593ee8632
David Barina <ibarina@fit.vutbr.cz>
2019-11-22 14:34:36 +0100

    server: allow sieve-32 mod12 checksums

--- src/server/server.c
+++ src/server/server.c
@@ -619,7 +619,7 @@ int read_message(int fd, int thread_id, const char *ipv4)
 			return -1;
 		}
 
-		if ((checksum>>23) != 196126) {
+		if ((checksum>>23) != 196126 && (checksum>>24) != 0x3354) {
 			message(ERR "suspicious checksum (%" PRIu64 ", 0x%" PRIx64 "), done in %" PRIu64 " secs, rejecting the result! (assignment %" PRIu64 ")\n",
 				checksum, checksum, user_time, n);
 			return -1;
