b132f8cf86b6d3a7363ba5ed94d44923a56044db
David Barina <ibarina@fit.vutbr.cz>
2019-11-22 15:38:20 +0100

    scripts: update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+export LANG=C
+
 export SERVER_NAME=pcbarina.fit.vutbr.cz
 
-HOSTNAME=$(hostname -s)
+HOSTNAME=$(hostname -s | tr '[:upper:]' '[:lower:]')
 
 TASK_UNITS=16
-
 if [[ "pco204-00" =~ ^pco204-..$ ]]; then
 	TASK_UNITS=20
 fi
 
-cd ~/collatz-${HOSTNAME,,}/src
+cd ~/collatz-${HOSTNAME}/src
 
 CC=gcc
 if type clang > /dev/null 2> /dev/null && clang --version | grep -q "version [89]"; then
@@ -18,8 +19,8 @@ if type clang > /dev/null 2> /dev/null && clang --version | grep -q "version [89
 	CC=clang
 fi
 
-make -C worker clean all CC=$CC USE_MOD12=0
-make -C gpuworker clean all TASK_UNITS=${TASK_UNITS} USE_LIBGMP=1 || echo "unable to build gpuworker"
+make -C worker clean all USE_LIBGMP=1 CC=$CC
+make -C gpuworker clean all USE_LIBGMP=1 TASK_UNITS=${TASK_UNITS} || echo "unable to build gpuworker"
 make -C mclient clean all
 
 cd mclient
