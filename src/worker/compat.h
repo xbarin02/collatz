08d97a13f17e96c3c998dfa2aad1a3a007e22a30
David Barina <ibarina@fit.vutbr.cz>
2019-11-23 14:38:13 +0100

    compat.h: use __builtin_trap() in future

--- src/worker/compat.h
+++ src/worker/compat.h
@@ -16,7 +16,7 @@ static int __builtin_ctzu64(uint64_t n)
 #ifdef __WIN32__
 		case sizeof(unsigned long long): return __builtin_ctzll((unsigned long long)n);
 #endif
-		default: assert(!"matching type");
+		default: assert(!"matching type"); /* __builtin_trap() in gcc 4.7 */
 	}
 }
 
@@ -30,7 +30,7 @@ static uint64_t atou64(const char *nptr)
 #ifdef __WIN32__
 		case sizeof(unsigned long long): return (uint64_t)strtoull(nptr, NULL, 10);
 #endif
-		default: assert(!"matching type");
+		default: assert(!"matching type"); /* __builtin_trap() in gcc 4.7 */
 	}
 }
 
