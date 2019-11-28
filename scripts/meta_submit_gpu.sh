648bb3d1ec0e2247fd4c0f1caf1995de2b94c896
David Barina <ibarina@fit.vutbr.cz>
2019-11-28 13:07:37 +0100

    metacentrum gpu do not work with clang

--- scripts/meta_submit_gpu.sh
+++ scripts/meta_submit_gpu.sh
@@ -66,7 +66,7 @@ cp -r "${SRCDIR}" .
 cd collatz/src
 
 # build mclient & gpuworker
-make -C gpuworker clean all CC=$CC
+make -C gpuworker clean all CC=gcc
 make -C mclient clean all
 
 pushd $MAPDIR
