f3966baeebc2584cfb03dff19f3537608737f95f
David Barina <ibarina@fit.vutbr.cz>
2019-11-30 15:45:56 +0100

    chk-tool: format

--- src/server/chk-tool.c
+++ src/server/chk-tool.c
@@ -309,7 +309,7 @@ int main(int argc, char *argv[])
 				overflow_sum += overflow;
 
 				if (++c < 32) {
-					printf("- #%2i: found %" PRIu64 " overflows on the assignment %" PRIu64 " (below %" PRIu64 " x 2^60)\n", c, overflow, n, (n >> 20) + 1);
+					printf("- #%2i: found %" PRIu64 " overflows on the assignment %9" PRIu64 " (below %" PRIu64 " x 2^60)\n", c, overflow, n, (n >> 20) + 1);
 				}
 			}
 		}
