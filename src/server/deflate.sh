05a880e3e8fb06aa17cd7aaeb8cd556cd0f257f7
David Barina <ibarina@fit.vutbr.cz>
2019-12-02 18:12:12 +0100

    server/deflate.sh now really works

--- src/server/deflate.sh
+++ src/server/deflate.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+read -p "Deflate (y/n)? " answer
+case ${answer:0:1} in
+	y|Y )
+		:
+	;;
+	* )
+		exit 1
+	;;
+esac
+
 du -sh *.map *.dat
 
+echo Deflating...
 for f in *.map *.dat; do
-	echo fallocate -d $f
+	fallocate -d $f
 done
 
 du -sh *.map *.dat
