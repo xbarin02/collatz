e57303f3ca6e9bae0dce4bf4291e5ff1a977abb3
David Barina <ibarina@fit.vutbr.cz>
2019-12-06 10:57:57 +0100

    compat.h: experimentally use __builtin_trap()

--- src/common/compat.h
+++ src/common/compat.h
@@ -15,7 +15,6 @@
 #endif
 
 #include <stdint.h>
-#include <assert.h>
 
 UNUSED HOT
 static int ctzu64(uint64_t n)
@@ -29,7 +28,7 @@ static int ctzu64(uint64_t n)
 #ifdef __WIN32__
 		case sizeof(unsigned long long): return __builtin_ctzll((unsigned long long)n);
 #endif
-		default: assert(!"matching type"); /* __builtin_trap() in gcc 4.7 */
+		default: __builtin_trap();
 	}
 }
 
@@ -43,7 +42,7 @@ static uint64_t atou64(const char *nptr)
 #ifdef __WIN32__
 		case sizeof(unsigned long long): return (uint64_t)strtoull(nptr, NULL, 10);
 #endif
-		default: assert(!"matching type"); /* __builtin_trap() in gcc 4.7 */
+		default: __builtin_trap();
 	}
 }
 
@@ -58,6 +57,8 @@ static int min(int a, int b)
 {
 	return a < b ? a : b;
 }
+
+#include <assert.h>
 
 UNUSED
 static uint64_t pow3u64(uint64_t n)
