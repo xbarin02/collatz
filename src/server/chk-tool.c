a0563e23727dcc9ddf82f862f5d1fb9fb26ebad2
David Barina <ibarina@fit.vutbr.cz>
2019-12-08 09:31:07 +0100

    chk-tool: esieve-2^32 sieve-3^1 support

--- src/server/chk-tool.c
+++ src/server/chk-tool.c
@@ -273,6 +273,9 @@ int main(int argc, char *argv[])
 
 		printf("esieve-2^16 checksums:\n");
 		print_checksum_stats_ex(0x83b, 28);
+
+		printf("esieve-2^32 sieve-3^1 checksums:\n");
+		print_checksum_stats(0x2a27);
 	}
 
 	/* missing checksums */
@@ -307,6 +310,7 @@ int main(int argc, char *argv[])
 		struct timerec tr_mod_2_16_e = timerec_create();
 		struct timerec tr_mod_2_32 = timerec_create();
 		struct timerec tr_mod_2_32_3_1 = timerec_create();
+		struct timerec tr_mod_2_32_3_1_e = timerec_create();
 
 		for (n = 0; n < ASSIGNMENTS_NO; ++n) {
 			uint64_t usertime = g_usertimes[n];
@@ -340,6 +344,10 @@ int main(int argc, char *argv[])
 			if (usertime != 0 && (checksum>>24) == 0x3354) {
 				ADD_TIME(tr_mod_2_32_3_1, usertime);
 			}
+
+			if (usertime != 0 && (checksum>>24) == 0x2a27) {
+				ADD_TIME(tr_mod_2_32_3_1_e, usertime);
+			}
 		}
 
 		printf("all user time records:\n");
@@ -362,6 +370,9 @@ int main(int argc, char *argv[])
 
 		printf("esieve-2^16 user time records:\n");
 		avg_and_print_usertime(tr_mod_2_16_e.total, tr_mod_2_16_e.count);
+
+		printf("esieve-2^32 sieve-3^1 user time records:\n");
+		avg_and_print_usertime(tr_mod_2_32_3_1_e.total, tr_mod_2_32_3_1_e.count);
 
 		printf("speedup (sieve-2^2 long/short) = %" PRIu64 "\n", round_div_ul(tr_mod_2_2_long.avg, tr_mod_2_2_short.avg));
 		printf("\n");
