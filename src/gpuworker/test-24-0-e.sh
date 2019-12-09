fa4cf8e4910e515a6879a0b0160b01f46d31fad3
David Barina <ibarina@fit.vutbr.cz>
2019-12-09 11:16:48 +0100

    gpuworker: test for esieve-2^24

---
+++ src/gpuworker/test-24-0-e.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+KERNEL=kernel32.cl
+SIEVE_LOGSIZE=24
+
+function check()
+{
+	T=$(./gpuworker -k "$KERNEL" $1)
+
+	CHECKSUM=$(echo "$T" | sed -unE '/CHECKSUM/s/.* (.*) .*/\1/p')
+	MAXIMUM_OFFSET=$(echo "$T" | sed -unE '/MAXIMUM_OFFSET/s/.* (.*)/\1/p')
+	MAXIMUM_CYCLE_OFFSET=$(echo "$T" | sed -unE '/MAXIMUM_CYCLE_OFFSET/s/.* (.*)/\1/p')
+	# '
+
+	if [[ "$T" =~ ABORTED_DUE_TO_OVERFLOW ]]; then
+		echo "ABORTED_DUE_TO_OVERFLOW"
+		return
+	fi
+
+	echo "$CHECKSUM $MAXIMUM_OFFSET $MAXIMUM_CYCLE_OFFSET"
+}
+
+function verify()
+{
+	echo -e "\e[1m$1\e[0m: checking..."
+
+	R="$(check $1)"
+
+	if test "$R" = ABORTED_DUE_TO_OVERFLOW; then
+		echo -e "\e[1m$1\e[0m: \e[31m$R\e[0m"
+	elif test "$R" = "$2"; then
+		echo -e "\e[1m$1\e[0m: \e[32mPASSED\e[0m"
+	else
+		echo -e "\e[1m$1\e[0m: \e[31mFAILED ($R)\e[0m"
+	fi
+}
+
+function build()
+{
+	echo make --quiet clean all $* CC=$CC
+	make --quiet clean all $* CC=$CC
+}
+
+CC=gcc
+if type clang > /dev/null 2> /dev/null && clang --version | grep -q "version [89]"; then
+        echo "INFO: clang available"
+        CC=clang
+fi
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999215 "390339659554 225865785455 774712573087"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999216 "390340546613 585714939119 179472374087"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999217 "390340530896 981603428123 625267373159"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999218 "390340864165 371145336991 152337526527"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999219 "390338509130 30069329055 759086898943"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999220 "390339732880 705488991903 72345484027"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999221 "390340305013 8275696879 589522022399"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999222 "390340375358 11710777343 319962915967"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999223 "390339126386 247028916391 195234121343"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999224 "390339733038 463055201307 950509349991"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 98999225 "390339002599 851511117927 18710076399"
+
+build SIEVE_LOGSIZE=${SIEVE_LOGSIZE} USE_ESIEVE=1 && verify 97064899 "0 0 0"
