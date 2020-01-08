443d49a4f18a9a6f1f4beea7e8c3f9fef2bde750
David Barina <ibarina@fit.vutbr.cz>
2020-01-08 16:09:59 +0100

    server: allow 0x27d8 checksums (esieve-2^34 and 3^1)

--- src/server/server.c
+++ src/server/server.c
@@ -476,7 +476,8 @@ int read_message(int fd, int thread_id, const char *ipv4)
 		    (checksum>>24) != 0x2a27 &&
 		    (checksum>>24) != 0x5ae2 &&
 		    (checksum>>24) != 0x5ae1 &&
-		    (checksum>>24) != 0x3c96) {
+		    (checksum>>24) != 0x3c96 &&
+		    (checksum>>24) != 0x27d8) {
 			message(ERR "suspicious checksum (%" PRIu64 ", 0x%" PRIx64 "), done in %" PRIu64 " secs, rejecting the result! (assignment %" PRIu64 ")\n",
 				checksum, checksum, user_time, n);
 			return -1;
