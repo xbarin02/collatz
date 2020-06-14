a8c09e7787198f846ce34553a663cd233666a60a
David Barina <ibarina@fit.vutbr.cz>
2020-06-14 17:40:12 +0200

    show: show maximum

--- src/server/show.c
+++ src/server/show.c
@@ -15,13 +15,42 @@ void mpz_pow3(mpz_t r, unsigned long n)
 	mpz_ui_pow_ui(r, 3UL, n);
 }
 
+void mpz_get_maximum(mpz_t max, const mpz_t n0)
+{
+	mpz_t n;
+
+	mpz_init_set(n, n0);
+	mpz_set(max, n);
+
+	while (mpz_cmp_ui(n, 1UL) != 0) {
+		if (mpz_congruent_ui_p(n, 1UL, 2UL)) {
+			/* odd step */
+			mpz_mul_ui(n, n, 3UL);
+			mpz_add_ui(n, n, 1UL);
+		} else {
+			/* even step */
+			mpz_fdiv_q_ui(n, n, 2UL);
+		}
+
+		if (mpz_cmp(n, max) > 0) {
+			mpz_set(max, n);
+		}
+	}
+
+	mpz_clear(n);
+}
+
 void report(mpz_t n)
 {
 	int i = 0;
 	mpz_t a;
 	mp_bitcnt_t Salpha = 0, Sbeta = 0;
+	mpz_t max;
 
 	mpz_init(a);
+	mpz_init(max);
+
+	mpz_get_maximum(max, n);
 
 	printf("<table>\n");
 	printf("<tr><th>#</th><th>$n$</th><th>$(\\alpha, \\beta)$</th></tr>\n");
@@ -53,9 +82,11 @@ void report(mpz_t n)
 	printf("<li>Steps: %lu\n", (unsigned long)(Salpha + Sbeta));
 	printf("<li>Odd steps: %lu\n", (unsigned long)Salpha);
 	printf("<li>Even steps: %lu\n", (unsigned long)Sbeta);
+	gmp_printf("<li>Maximum: %Zi\n", max);
 	printf("</ul>\n");
 
 	mpz_clear(a);
+	mpz_clear(max);
 }
 
 int main(int argc, char *argv[])
