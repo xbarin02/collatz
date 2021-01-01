1c2b921a59bd2d83afdbf430730ddf47982cc69f
David Barina <ibarina@fit.vutbr.cz>
2021-01-01 10:56:32 +0100

    cleanup in verify-result

--- src/server/verify-result.c
+++ src/server/verify-result.c
@@ -51,35 +51,6 @@ const uint64_t *g_mxoffsets = 0;
 
 uint64_t g_lut64[LUT_SIZE64];
 
-static uint128_t g_max = 0;
-static uint128_t g_max_n0 = 0;
-
-#ifdef _USE_GMP
-static void mpz_pow3(mpz_t r, unsigned long n)
-{
-	mpz_ui_pow_ui(r, 3UL, n);
-}
-#endif
-
-#ifdef _USE_GMP
-void mpz_mul_pow3(mpz_t r, mp_bitcnt_t alpha)
-{
-	mpz_t a;
-
-	assert(alpha <= ~0UL);
-
-	mpz_init(a);
-
-	/* 3^alpha */
-	mpz_pow3(a, (unsigned long)alpha);
-
-	/* r *= 3^alpha */
-	mpz_mul(r, r, a);
-
-	mpz_clear(a);
-}
-#endif
-
 void init_lut()
 {
 	int a;
@@ -88,13 +59,6 @@ void init_lut()
 		g_lut64[a] = pow3u64((uint64_t)a);
 	}
 }
-
-#ifdef _USE_GMP
-static mp_bitcnt_t mpz_ctz(const mpz_t n)
-{
-	return mpz_scan1(n, 0);
-}
-#endif
 
 #ifdef _USE_GMP
 static void mpz_init_set_u128(mpz_t rop, uint128_t op)
@@ -109,158 +73,6 @@ static void mpz_init_set_u128(mpz_t rop, uint128_t op)
 	mpz_add_ui(rop, rop, (unsigned long)nl);
 }
 #endif
-
-#ifdef _USE_GMP
-mpz_t g_mpz_max;
-uint128_t g_mpz_max_n0;
-#endif
-
-static void mpz_check2(uint128_t n0_, uint128_t n_, int alpha_)
-{
-#ifdef _USE_GMP
-	mp_bitcnt_t alpha, beta;
-	mpz_t n;
-	mpz_t n0;
-
-	printf("OVERFLOW MPZ @ n0=0x%016" PRIx64 ":%016" PRIx64 " (not maximum)\n", (uint64_t)(n0_>>64), (uint64_t)n0_);
-
-	assert(alpha_ >= 0);
-	alpha = (mp_bitcnt_t)alpha_;
-
-	mpz_init_set_u128(n, n_);
-	mpz_init_set_u128(n0, n0_);
-
-	do {
-		/* n *= 3^alpha */
-		mpz_mul_pow3(n, alpha);
-
-		/* n-- */
-		mpz_sub_ui(n, n, 1UL);
-
-		/* handle maximum */
-		if (mpz_cmp(n, g_mpz_max) > 0) {
-			mpz_set(g_mpz_max, n);
-			g_mpz_max_n0 = n0_;
-		}
-
-		beta = mpz_ctz(n);
-
-		/* n >>= ctz(n) */
-		mpz_fdiv_q_2exp(n, n, beta);
-
-		if (mpz_cmp(n, n0) < 0) {
-			break;
-		}
-
-		/* n++ */
-		mpz_add_ui(n, n, 1UL);
-
-		alpha = mpz_ctz(n);
-
-		/* n >>= alpha */
-		mpz_fdiv_q_2exp(n, n, alpha);
-	} while (1);
-
-	mpz_clear(n);
-	mpz_clear(n0);
-#else
-	(void)n0_;
-	(void)n_;
-	(void)alpha_;
-
-	abort();
-#endif
-}
-
-static void check2(uint128_t n0, uint128_t n, int alpha)
-{
-	printf("OVERFLOW 128 @ n0=0x%016" PRIx64 ":%016" PRIx64 " (not maximum)\n", (uint64_t)(n0>>64), (uint64_t)n0);
-
-	do {
-		if (n > UINT128_MAX >> 2*alpha) {
-			mpz_check2(n0, n, alpha);
-			return;
-		}
-
-		do {
-			int alpha0 = min(alpha, LUT_SIZE64 - 1);
-			n *= g_lut64[alpha0];
-			alpha -= alpha0;
-		} while (alpha > 0);
-
-		n--;
-
-		if (n > g_max) {
-			g_max = n;
-			g_max_n0 = n0;
-		}
-
-		do {
-			n >>= ctzu64((uint64_t)n);
-		} while (!(n & 1));
-
-		if (n < n0) {
-			return;
-		}
-
-		n++;
-
-		alpha = 0;
-		do {
-			alpha += ctzu64((uint64_t)n);
-		} while (!(n & 1));
-
-		n >>= alpha;
-	} while (1);
-}
-
-static void check(uint128_t n)
-{
-	const uint128_t n0 = n;
-	int alpha;
-
-	if (n == UINT128_MAX) {
-		check2(n0, UINT128_C(1), 128);
-		return;
-	}
-
-	do {
-		n++;
-
-		alpha = 0;
-		do {
-			alpha += ctzu64((uint64_t)n);
-		} while (!(n & 1));
-
-		n >>= alpha;
-
-		if (n > UINT128_MAX >> 2*alpha) {
-			check2(n0, n, alpha);
-			return;
-		}
-
-		do {
-			int alpha0 = min(alpha, LUT_SIZE64 - 1);
-			n *= g_lut64[alpha0];
-			alpha -= alpha0;
-		} while (alpha > 0);
-
-		n--;
-
-		if (n > g_max) {
-			g_max = n;
-			g_max_n0 = n0;
-		}
-
-		do {
-			n >>= ctzu64((uint64_t)n);
-		} while (!(n & 1));
-
-		if (n < n0) {
-			return;
-		}
-	} while (1);
-}
 
 uint128_t get_max(uint128_t n0)
 {
@@ -306,9 +118,6 @@ int main(int argc, char *argv[])
 {
 	uint64_t task_id = (argc > 1) ? atou64(argv[1]) : 0;
 	uint64_t task_size = TASK_SIZE;
-	uint128_t n;
-	uint128_t n_min;
-	uint128_t n_sup;
 	uint64_t checksum;
 	uint64_t mxoffset;
 #ifdef _USE_GMP
@@ -346,47 +155,12 @@ int main(int argc, char *argv[])
 
 	assert((uint128_t)task_id <= (UINT128_MAX >> task_size));
 
-	/* n of the form 4n+3 */
-	n_min = ((uint128_t)(task_id + 0) << task_size) + 3;
-	n_sup = ((uint128_t)(task_id + 1) << task_size) + 3;
-
 	printf("RANGE 0x%016" PRIx64 ":%016" PRIx64 " 0x%016" PRIx64 ":%016" PRIx64 "\n",
 		(uint64_t)(((uint128_t)(task_id + 0) << task_size)>>64),
 		(uint64_t)(((uint128_t)(task_id + 0) << task_size)    ),
 		(uint64_t)(((uint128_t)(task_id + 1) << task_size)>>64),
 		(uint64_t)(((uint128_t)(task_id + 1) << task_size)    )
 	);
-
-#ifdef _USE_GMP
-	mpz_init_set_ui(g_mpz_max, 0UL);
-	g_mpz_max_n0 = 0;
-#endif
-
-	printf("[DEBUG] computing checksum...\n");
-
-	for (n = n_min; n < n_sup; n += 4) {
-		check(n);
-	}
-
-#ifdef _USE_GMP
-	if (1) {
-		mpz_t max, max_n0;
-
-		mpz_init_set_u128(max, g_max);
-
-		if (g_mpz_max_n0 && mpz_cmp(max, g_mpz_max) < 0) {
-			mpz_set(max, g_mpz_max);
-			mpz_init_set_u128(max_n0, g_mpz_max_n0);
-		} else {
-			mpz_init_set_u128(max_n0, g_max_n0);
-		}
-
-		gmp_printf("MAXIMUM %Zd n0=%Zd (may differ from the original Collatz function)\n", max, max_n0);
-
-		mpz_clear(max_n0);
-		mpz_clear(max);
-	}
-#endif
 
 	return 0;
 }
