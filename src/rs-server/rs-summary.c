1173332aa3a58e7df7cc31e9401b51a20668ec56
David Barina <ibarina@fit.vutbr.cz>
2025-11-13 10:38:22 +0100

    print average time

--- src/rs-server/rs-summary.c
+++ src/rs-server/rs-summary.c
@@ -52,6 +52,7 @@ int main(/*int argc, char *argv[]*/)
 	uint64_t usertime = 0;
 	int complete = 1;
 	uint64_t num_complete = 0;
+	uint64_t num_time = 0;
 
 	printf("TARGET = %i\n", TARGET);
 	printf("ASSIGNMENTS_NO = %" PRIu64 "\n", ASSIGNMENTS_NO);
@@ -72,13 +73,21 @@ int main(/*int argc, char *argv[]*/)
 		} else {
 			num_complete++;
 		}
+
+		if (usertime_) {
+			num_time++;
+		}
 	}
 
 	printf("TOTAL CHECKSUM %" PRIu64 "\n", checksum);
 	printf("TOTAL TIME %" PRIu64 " ms\n", usertime);
 	printf("TOTAL TIME %" PRIu64 " (%" PRIu64 ":%02" PRIu64 ":%02" PRIu64 ")\n",
-			(usertime + 500)/1000,
-			(usertime + 500)/1000/60/60, (usertime + 500)/1000/60%60, (usertime + 500)/1000%60);
+		(usertime + 500)/1000,
+		(usertime + 500)/1000/60/60, (usertime + 500)/1000/60%60, (usertime + 500)/1000%60);
+
+	printf("avg. time: %f (%" PRIu64 ":%02" PRIu64 ":%02" PRIu64 ")\n", usertime / (double)num_time / 1000,
+		(usertime + 500)/1000/num_time/60/60, (usertime + 500)/1000/num_time/60%60, (usertime + 500)/1000/num_time%60
+	);
 
 	printf("all assignments are complete: %i\n", complete);
 	printf("number of completed assignments: %" PRIu64 "\n", num_complete);
