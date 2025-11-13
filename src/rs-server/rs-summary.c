350ebbbde8eb309cb2ce62b26f9b8de9838ad49f
David Barina <ibarina@fit.vutbr.cz>
2025-11-13 19:51:59 +0100

    rs-summary: print old and new limit

--- src/rs-server/rs-summary.c
+++ src/rs-server/rs-summary.c
@@ -14,11 +14,70 @@
 #include "wideint.h"
 #include "compat.h"
 
+#include <assert.h>
+
 #include "rs-config.h"
 
 #define ASSIGNMENTS_NO (UINT64_C(1) << (LOG2_NO_PROCS))
 
 #define RECORDS_SIZE (ASSIGNMENTS_NO * 8)
+
+uint128_t g_pow3[64];
+
+uint128_t pow3u128(uint128_t n)
+{
+	uint128_t r = 1;
+	uint128_t b = 3;
+
+	while (n) {
+		if (n & 1) {
+			assert(r <= UINT128_MAX / b);
+			r *= b;
+		}
+
+		assert(b <= UINT128_MAX / b);
+		b *= b;
+
+		n >>= 1;
+	}
+
+	return r;
+}
+
+void pow3_init()
+{
+	int i = 0;
+	for (; i < 64; ++i) {
+		g_pow3[i] = pow3u128(i);
+	}
+}
+
+void print(uint128_t n)
+{
+	char buff[40];
+	char r_buff[40];
+
+	char *ptr = buff;
+
+	int i = 0;
+
+	while (n != 0) {
+		*(ptr++) = '0' + n % 10;
+		n /= 10;
+	}
+
+	*ptr = 0;
+
+	ptr--;
+
+	while (ptr >= buff) {
+		r_buff[i++] = *(ptr--);
+	}
+
+	r_buff[i] = 0;
+
+	puts(r_buff);
+}
 
 const uint64_t *open_records(const char *path)
 {
@@ -56,6 +115,8 @@ int main(/*int argc, char *argv[]*/)
 	uint64_t num_complete = 0;
 	uint64_t num_time = 0;
 
+	pow3_init();
+
 	printf("TARGET = %i\n", TARGET);
 	printf("ASSIGNMENTS_NO = %" PRIu64 "\n", ASSIGNMENTS_NO);
 	printf("LOG2_NO_PROCS = %i\n", LOG2_NO_PROCS);
@@ -83,6 +144,12 @@ int main(/*int argc, char *argv[]*/)
 			num_time++;
 		}
 	}
+
+	printf("OLD_LIMIT (all numbers below this must be already verified) ");
+	print(4 * g_pow3[TARGET + 0] + 2);
+
+	printf("NEW LIMIT (all numbers below this are now verified) ");
+	print(4 * g_pow3[TARGET + 1] + 2);
 
 	printf("TOTAL CHECKSUM %" PRIu64 "\n", checksum);
 	printf("TOTAL TIME %" PRIu64 " ms\n", usertime);
