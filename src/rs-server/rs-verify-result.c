97e449a7b438b7b55363f4ef81e33f562becdaaf
David Barina <ibarina@fit.vutbr.cz>
2025-11-13 10:55:31 +0100

    print overflows

--- src/rs-server/rs-verify-result.c
+++ src/rs-server/rs-verify-result.c
@@ -44,18 +44,21 @@ const uint64_t *open_records(const char *path)
 
 const uint64_t *g_checksums = 0;
 const uint64_t *g_usertimes = 0;
+const uint64_t *g_overflows = 0;
 
 int main(int argc, char *argv[])
 {
 	uint64_t task_id = (argc > 1) ? atou64(argv[1]) : 0;
 	uint64_t checksum;
 	uint64_t usertime;
+	uint64_t overflow;
 
 	printf("TASK_ID %" PRIu64 "\n", task_id);
 	assert(task_id < ASSIGNMENTS_NO);
 
 	g_checksums = open_records("checksums.dat");
 	g_usertimes = open_records("usertimes.dat");
+	g_overflows = open_records("overflows.dat");
 
 	checksum = g_checksums[task_id];
 
@@ -68,6 +71,10 @@ int main(int argc, char *argv[])
 	printf("TIME %" PRIu64 " (%" PRIu64 ":%02" PRIu64 ":%02" PRIu64 ")\n",
 			(usertime + 500)/1000,
 			(usertime + 500)/1000/60/60, (usertime + 500)/1000/60%60, (usertime + 500)/1000%60);
+
+	overflow = g_overflows[task_id];
+
+	printf("OVERFLOWS: %" PRIu64 "\n", overflow);
 
 	return 0;
 }
